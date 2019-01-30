d050f9701025a081934908b18fb65715f9d0a9b2
Darriel Bleasdille <42749778+darrielb23@users.noreply.github.com>
2019-01-30 17:41:52 -0500

    commit t o master
    chnges made

---
+++ sketch.js
@@ -0,0 +1,34 @@
+/*
+Story
+by Darriel
+rough draft
+mmp310 week1
+*/
+
+
+
+function preload () {
+
+}
+
+
+
+function setup() {
+    createCanvas(900, 500);
+
+}
+
+function draw() {
+    background(75);
+
+
+
+
+
+
+
+
+
+
+
+}

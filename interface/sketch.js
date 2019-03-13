ab6b37060970085a20238207205041f9e1c1d6dd
darriel <darrielb23@gmail.com>
2019-03-13 19:12:57 -0400

    commit to master
    changes made

--- interface/sketch.js
+++ interface/sketch.js
@@ -1,26 +1,66 @@
-/*
-	Interface
-	Week  mmp 310
-dom library ui
-*/
+
+/* global variables */
+var bg = "green";
+var g = 255;
+var columns = 8;
+var rows = 8;
+var sw = 25;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(255);
+	createCanvas(windowWidth, windowHeight/2);
+	pattern();
+
+	var button = createButton("Save Image");
+	button.mousePressed(saveImage);
+//	button.style('color', bg);
+	button.style('font-family', 'monaco');
+	button.id('my-button');
+	button.class('interface');
+//	button.position(0, height - 50);
+	
+	var generatePattern = createButton("Generate Pattern");
+	generatePattern.mousePressed(pattern);
+	generatePattern.class('interface');
+//	generatePattern.position(180, height - 50);
+	
+	// columns slider
+	createDiv("Number of Columns");
+	var columnSlider = createSlider(8, 100, columns);
+	columnSlider.input(setColumn);
+	
+	// rows 
+	
+	// bg color
+	createDiv("Background Color");
+	var colorSlider = createSlider(0, 255, g);
+	colorSlider.input(setColor);
 	
 	
-	var columns = 8;
-	var rows = 8;
-	var w = width / columns; // column width
-	var h = height / rows; // row height
+}
+
+function setColumn() {
+	columns = this.value();
+    sw = map(this.value(), 8, 100, 20, 8);
+	pattern();
+}
+
+function setColor() {
+	g = this.value();
+	pattern();
+}
+
+function pattern() {
+	background(0, g, 127); \\ color bk
 	
-	// nested for loop 
+	var w = width / columns;
+	var h = height / rows;
+	
 	for (let x = 0; x <= width; x += w) {
 		for (let y = 0; y <= height; y += h) {
 			
-		
-
-			strokeWeight(35);
+			// drawing 
+			
+			strokeWeight(sw);
 			var r = random(4);
 			if (r > 3) {
 
@@ -61,23 +101,22 @@ function setup() {
             fill("yellow");
                  ellipse(x + w/4, y + h - h/4, w/2, h/2); //bottom left 
                 
-
-                
-                
-                
 				
 			}
-			}
 			
+			// end drawing
 		}
 	}
-
-
-
-// click to save image to Downloads folder
-function mouseClicked() {
-	save('lattern-1.jpg');
 }
+
+function saveImage() {
+	save("pattern.png");
+}
+
+
+
+
+
 
 
 
